10802012f8ef37979431f45f1e95c5095abf018d
Nikolai Kiselev <kisnikn@mail.ru>
2020-11-04 00:59:50 +0300

    format code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,17 +1,17 @@
 public class CreditPaymentService {
-    int calculate (int total, int month, double percent) {
-       int payment = 0;
+    int calculate(int total, int month, double percent) {
+        int payment = 0;
 
-       //процентная ставка на 1 месяц
-        double percentm = percent/12;
+        //процентная ставка на 1 месяц
+        double percentm = percent / 12;
         //вспомогательное вычисление для формулы расчета платежа
-        double tmp = Math.pow(1+percentm, month)-1;
+        double tmp = Math.pow(1 + percentm, month) - 1;
 
         //расчет платежа
-        payment = (int) (total*(percentm+percentm/tmp));
+        payment = (int) (total * (percentm + percentm / tmp));
 
 
-       return payment;
+        return payment;
     }
 
 }
